490c5bebd81c5e5865545582b94c865137e08073
reddysai <reddysai@agiratech.com>
2017-01-09 17:11:22 +0530

    closes #3 added data pre processing of review text

---
+++ data_pre_processing.py
@@ -0,0 +1,61 @@
+import json, re
+from nltk.tokenize import word_tokenize
+
+class Read_Data:
+
+  def read_topics(self, infile):
+    topics = []
+    topic = []
+    f = open(infile, 'r')
+    for line in f:
+      line = line.strip()
+      if line != "":
+        topic.append(line)
+      else:
+        topics.append(topic)
+        topic = []
+    topics.append(topic)
+    return topics
+
+class Preprocessing:
+
+
+  # Underscores multiword topics and optionally removes words
+  # in size-n window around topics to increase conditional independence
+  # of topics and the text.  Default window size is 0
+
+  def process_anchors(self, infile, topicfile, window_size=0):
+    outfile = infile.replace("selected_", "bigram_")
+
+    topics = Read_Data().read_topics(topicfile)
+    topic_list = []
+    for topic in topics:
+      for a in topic:
+        topic_list.append(a)
+    topics = topic_list
+
+    with open(infile) as file, open(outfile, 'w') as newfile:
+      for line in file:
+        data = json.loads(line)
+        review = data["text"]
+        review = review.lower()
+        review = review.replace("'", "")
+        review = re.sub("[^a-z]+", " ", review)
+
+        for word in topics:
+          ##searching bigrams in review text
+          if " "+word+" " in review:
+            if " " in word:
+              oldword = word
+              word = word.replace(" ", "_")
+              review = review.replace(" "+oldword+" ", " "+word+" ")
+              review = word_tokenize(review)
+              ind = review.index(word)
+              review[ind] = word
+              review = " ".join(review[:ind-window_size]+[word]+review[(ind+1)+window_size:])
+
+        data["text"] = review
+        json.dump({'business_id':data['business_id'],  'user_id': data['user_id'], 'stars':data['stars'], 'text':data['text']}, newfile, ensure_ascii=True)
+        newfile.write('\n')
+
+    return outfile
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import get_restaurant_review_data as rd
-
+import data_pre_processing as dpp
 rd = rd.Get_restaurant_reviews()
-
+dpp = dpp.Preprocessing()
 path = "scripts/data_files"
 city = "Phoenix"
 category = "Mexican"
+topic = path+"/topic.txt"
 
-rd.get_restaurant_reviews(path, city, category, True, True)
+dataFile = rd.get_restaurant_reviews(path, city, category, True, True)
+dpp.process_anchors(dataFile, topic)
